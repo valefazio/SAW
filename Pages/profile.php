cf6bd1a623457203ea387956558cb1110a3ba0e0
giampyhatesyou <andrea.giampietro68@gmail.com>
2024-03-31 20:42:19 +0200

    demo profile.php

---
+++ Pages/profile.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Profile</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../Management/Style/profile.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <nav id="navbar">
+        <?php
+        include ("../Management/navbar.html");
+        ?>
+    </nav>
+    <h1>Profile</h1>
+    <?php
+        include ("../Management/accessControl.php");
+
+        if(!isLogged()) {
+            echo "<script> window.location.href = 'Access/login.php';</script>";
+            exit;
+        }
+        
+        $conn = accessDb();
+        $sql = "SELECT * FROM users WHERE email = " . $_SESSION["email"];
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo '<div class="profile-card">';
+                echo '<img class="images" src="data:image/jpeg;base64,' . base64_encode($row["profile_picture"]) . '" alt="Foto Profilo ' . $row["firstname"] . ' ' . $row["lastname"] . '">';
+                echo '<div class="profile-info">';
+                echo '<h2>' . $row["firstname"] . ' ' . $row["lastname"] . '</h2>';
+                echo '<p>Email: ' . $row["email"] . '</p>';
+                echo '<p>Recensioni: ' . $row["reviews"] . '</p>';
+                echo '</div>';
+                echo '</div>';
+            }
+        } else {
+            echo "Nessun risultato trovato";
+        }
+    ?>
+       
+ 
