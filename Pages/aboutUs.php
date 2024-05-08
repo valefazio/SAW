118ffa1695b1701b51c457064fb98c52cb3cf608
valefazio <vale.fazio.2002@gmail.com>
2024-05-08 17:40:33 +0200

    boh

--- Pages/aboutUs.php
+++ Pages/aboutUs.php
@@ -5,7 +5,7 @@
 	<title>About Us</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../Management/Style/constants.css">
+	<link rel="stylesheet" type="text/css" href="../Management/Style/constants.css">
 </head>
 
 <body>
@@ -14,12 +14,23 @@
 		include ("../Management/navbar.html");
 		?>
 	</nav>
-	<div id="" style="">
-		<h1>About Us</h1>
-		<p>The page you are looking for could not be found.</p>
+	<h1>About Us</h1>
+	<div id="" style="padding: 0 20px; line-height: 1.8; text-align: center">
+		<p>La città di <b>Mostropoli</b>, abitata da <i>mostri</i> di ogni genere, ricava l'elettricità di cui ha
+			l'esigenza dalla centrale elettrica
+			cittadina, la "Monsters & Co.", che converte le <b>urla</b> (SCREAM) dei bambini umani in energia elettrica.
+			I mostri che vi
+			lavorano devono spaventare i bambini,
+			allo scopo di farli urlare per la paura, <u>giungendo nelle loro camere di notte attraverso delle porte
+				speciali</u>, che sono l'unico collegamento tra
+			il mondo dei mostri e quello degli esseri umani.
+		</p>
+		<img src="../Management/Images/background.jpg" alt="Doors and monsters"
+			style="width: 100%; max-width: 1000px; height: auto;">
 	</div>
 	<?php
 	include ("../Management/footer.html");
 	?>
 </body>
+
 </html>
--- Pages/home.php
+++ Pages/home.php
@@ -15,37 +15,42 @@
 		?>
 	</nav>
 	<div class="header">
-		<div class="text">
-			<span style="font-size: 120px;">Find Your Next</span>
-			<span style="font-size: 135px;">Victim</span>
-		</div>
+		<p id="text"></p>
 	</div>
 	<div id="door_home">
 		<?php
 		include ("../Management/showHouses.php");
 		?>
 	</div>
-	<div class="website_description">
-		<div class="description">
-			<h1>What is SCREAM?</h1>
-			<p>SCREAM is a website that allows you to find the best places to visit in the world. You can search for
-				destinations, filter them by location, number of calendars and reviews, and find the best place for your
-				next
-				vacation. You can also find the best employees of the month and the most liked destinations by other
-				users.</p>
-		</div>
-		<div class="description">
-			<h1>How does it work?</h1>
-			<p>SCREAM is a website that allows you to find the best places to visit in the world. You can search for
-				destinations, filter them by location, number of calendars and reviews, and find the best place for your
-				next
-				vacation. You can also find the best employees of the month and the most liked destinations by other
-				users.</p>
-		</div>
-	</div>
 	<?php
 	include ("../Management/footer.html");
 	?>
 </body>
+
+<script>
+	// Text for typing effect
+const text = "Find Your Next Victim";
+const typingSpeed = 100; // Typing speed in milliseconds
+
+// Get the text element
+const textElement = document.getElementById('text');
+
+// Function to simulate typing effect
+function typeText(index) {
+    if (index < text.length) {
+        textElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(function() {
+            typeText(index);
+        }, typingSpeed);
+    }
+}
+
+// Start the typing effect when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    typeText(0);
+});
+
+</script>
 
 </html>
