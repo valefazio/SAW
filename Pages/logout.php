a43dddfd585375b0796156bdca92d67420b9bcb1
Agostino <araghiago@gmail.com>
2024-06-03 18:00:24 +0200

    fixato la logout

--- Pages/logout.php
+++ Pages/logout.php
@@ -3,12 +3,6 @@
 	include("../Management/utility.php");
     include("../Management/Database/connection.php");
     $email = $_SESSION['email'];
-    if(updateDb("users", ["remember_token"], [NULL], ["email"], [$email]) == false)
-        relocation("404.php");
-    if(updateDb("users", ["remember_token_created_at"], [NULL], ["email"], [$email]) == false)
-        relocation("404.php");
-    unset($_COOKIE['remember-me']);
-    setcookie('remember-me', '', time() - 3600, '/');
     unset($_SESSION['email']);
     session_destroy();
     header("Location: home.php");
